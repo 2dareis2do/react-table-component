03e62025e9959ab17baaa661709823eba4820b23
dan <info@danlobo.co.uk>
2017-09-29 12:51:03 +0100

    tidy up slightly

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ const rootAPI = ['https://swapi.co/api/?format=json'];
 const peopleAPI = ['https://swapi.co/api/people/?format=json'];
 const filmsAPI = ['https://swapi.co/api/films/?format=json'];
 
-
 export default class App extends React.Component {
 
     render() {
