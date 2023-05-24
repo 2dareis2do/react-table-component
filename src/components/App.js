292550a4ab5de3e899e9bb9259d1f132b073092c
Daniel Lobo <info@danlobo.co.uk>
2023-05-24 17:23:11 +0100

    - change api endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import React from 'react';
 import Layout from './Layout.js'
 import People from './People.js'
 
-const rootAPI = ['https://swapi.co/api/?format=json'];
-const peopleAPI = ['https://swapi.co/api/people/?format=json'];
-const filmsAPI = ['https://swapi.co/api/films/?format=json'];
+// const rootAPI = ['https://swapi.dev/api/?format=json'];
+const peopleAPI = ['https://swapi.dev/api/people/?format=json'];
+const filmsAPI = ['https://swapi.dev/api/films/?format=json'];
 
 export default class App extends React.Component {
 
